e8e57798971f0ce4085a0013da4c023d43b340bb
Michal Atlas <misa.31415@gmail.com>
2020-10-03 11:45:18 +0200

    Door now sets player Coordinates

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,16 +1,18 @@
 extends Area2D
 
 export var NewScenePath = "";
-
+export var TargetCoords = Vector2(0,0);
 func _ready():
 	pass
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_up") && get_tree().get_root().get_node("Scene/Player") in get_overlapping_bodies():
+	if Input.is_action_just_pressed("ui_up") && $"/root/Player" in get_overlapping_bodies():
+		$"/root/Player".position.x = TargetCoords.x;
+		$"/root/Player".position.y = TargetCoords.y;
 		get_tree().change_scene(NewScenePath);
 		
 func _process(delta):
-	if get_tree().get_root().get_node("Scene/Player") in get_overlapping_bodies():
+	if get_tree().get_root().get_node("Player") in get_overlapping_bodies():
 		get_node("../ButtonLabel").visible = true;
 	else:
 		get_node("../ButtonLabel").visible = false;
