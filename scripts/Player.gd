8ed3abd90ee4775ec23e952a381e065694a2fc65
Michal Atlas <misa.31415@gmail.com>
2020-10-03 13:23:50 +0200

    Added Player Animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,15 +3,18 @@ extends KinematicBody2D
 const UP = Vector2(0,-1);
 var movement = Vector2(0,0);
 
-const SPEED = 100;
+const SPEED = 200;
 
 func _ready():
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_pressed("ui_right"):
 		movement.x = SPEED;
 	elif Input.is_action_pressed("ui_left"):
 		movement.x = -SPEED;
 	movement.x -= 5*sign(movement.x);
+	
+	$AnimatedSprite.flip_h = true if sign(movement.x)==1 else false;
+	$AnimatedSprite.animation = "idle" if abs(movement.x) < 20 else "run";
 	movement = move_and_slide(movement, UP);
