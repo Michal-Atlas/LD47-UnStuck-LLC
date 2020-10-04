67b5f228a688c7d442afff3af6ddc4d1ef37df72
Michal Atlas <misa.31415@gmail.com>
2020-10-04 20:40:09 +0200

    Added Necessary Globals

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -6,3 +6,28 @@ var Bill = [[8,"Work"],[-5,"Beer"], [-3,"Wife"]];
 var Beard = 2;
 var Beer = false;
 var Food = false;
+var Wife = 3;
+var BeerDays = 0;
+
+var PlayerSpawnPointX = 0;
+var WorkedToday = false;
+
+func clear_day():
+	for i in $"/root/Globals".Bill:
+		$"/root/Globals".Money += i[0];
+	$"/root/Globals".Bill = []
+	$"/root/Globals".Beard += 1;
+	
+	if Beer and not Food:
+		BeerDays += 1;
+	if BeerDays >= 5:
+		get_tree().change_scene("res://Endings/END_L8p.tscn");
+	if Wife <= 0:
+		get_tree().change_scene("res://Endings/END_Divorce.tscn");
+	if Wife >= 5:
+		get_tree().change_scene("res://Endings/END_Happy.tscn");
+	if Money <= 0:
+		get_tree().change_scene("res://Endings/END_Street.tscn")
+	
+	Food = false;
+	Beer = false;
