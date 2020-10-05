a3aceaf92cde877fa4f3441c88b3a450eece4f24
Michal Atlas <misa.31415@gmail.com>
2020-10-05 11:12:58 +0200

    Minor shop advancements

--- scripts/Shop.gd
+++ scripts/Shop.gd
@@ -3,3 +3,6 @@ extends Node2D
 func _ready():
 	$Aisle/Shelves/Shop_Shelf/AnimatedSprite.frame = 1;
 	$Aisle/Shelves/Shop_Shelf3/AnimatedSprite.frame = 1;
+func _exit_tree():
+	if $"/root/Globals".ShoppedToday and not $"/root/Globals".Food:
+		$"/root/Globals".Wife -= 1;
