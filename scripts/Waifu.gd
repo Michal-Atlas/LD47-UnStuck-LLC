05cb8ceb43681c8f1e228a9582536f6f801da0a4
Michal Atlas <misa.31415@gmail.com>
2020-10-05 15:46:37 +0200

    Waifu Dialogs

--- scripts/Waifu.gd
+++ scripts/Waifu.gd
@@ -1,9 +1,37 @@
 extends Node2D
 
+
+var rng = RandomNumberGenerator.new()
 onready var player = get_tree().get_nodes_in_group("Player")[0];
+var evening_dialogs = [
+	[],#0 empty
+	[],#1
+	[],#2
+	[],#3
+	["Welcome back home TP!","Welcome home!","Good evening!"],#4
+	["Welcome back home TP! How was work?", "Welcome back home TP! How was your day?", "Good evening TP! How was the day?" ],#5
+	["Good evening TP, did everything go well today?"],#6
+	["Good evening darling, how was your day?","Good evening darling, thank you so muc for yesterday, dinner was amazing!","Welcome back home darling, everything alright in work today?"],#7
+]
+
+var morning_dialogs_good = ["Good morning! Getting ready to go to work?",
+"Good morning TP, did you sleep well?", 
+"Good morning TP. You're taking a day off today?"
+]
+var morning_dialogs_bad = ["Shouldn't you be on your way to work?",
+ "You could at least go do something with your life and go to work.", "Morning..."
+	
+]
 
 func _ready():
-	cowsay("You're late!");
+	rng.randomize()
+	if $"/root/Globals".WorkedToday:
+		cowsay(evening_dialogs[$"/root/Globals".Wife][rng.randi_range(0,len(evening_dialogs[$"/root/Globals".Wife]))]);
+	elif !$"/root/Globals".WorkedToday and $"/root/Globals".Wife <= 4:
+		cowsay(morning_dialogs_bad[rng.randi_range(0,len(morning_dialogs_bad))])
+	else:
+		cowsay(morning_dialogs_good[rng.randi_range(0,len(morning_dialogs_good))])
+		
 	if $"/root/Globals".Wife < 3:
 		$AnimatedSprite.animation = "angry";
 	else:
